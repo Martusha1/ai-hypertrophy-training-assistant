537901d91f2b40e1560c21de11485b6d7d111dad
Martin Ivanov <martusha@Martins-MacBook-Air.local>
2026-09-06 21:19:09 +0300

    Plan command handlers

--- phase1/hippity.py
+++ phase1/hippity.py
@@ -7,24 +7,41 @@ bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hi!")
+# update is an object that holds all data coming from Telegram
+# context is an object that holds data about the status of the library
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # possible first message for when user opens bot for the first time
+    await update.message.reply_text("""Hello there, friend! How can I help with your training?
+    Please refer to /help if you want a brief outlook on what I can do.""")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("""Here are all commands you can use in Hippity:
+    /start : activates Hippity
+    /help : lists all possible commands in Hippity
+    /info : describes Hippity's purpose
+    /log : allows you to log your workout data
+    /progress <exercise> : checks for progression in an exercise of your choice
+    /program : show the current program you follow
+    /all_programs: lists all your programs
+    /new_program : Hippity generates a new program based on your needs""")
+
+# summarizes the bot's purpose in short
+async def info_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("""Hi! My name is Hippity and I am your personal
+    AI-powered hypertrophy assistant! I can hold records of your workout data and
+    analyze trends within your performance to help you overcome your current challenges
+    in the gym strictly based on current scientific evidence. Feel free to ask about
+    anything that's on your mind about muscle building and I will do my best to inform you.""")
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help comes!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
 
 def main():
     application = Application.builder().token(bot_token).build()
 
     application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(CommandHandler("info", info_command))
     application.add_handler(CommandHandler("help", help_command))
 
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
